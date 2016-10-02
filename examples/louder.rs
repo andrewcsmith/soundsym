6d337175dd14d8ea16f346a2ea59be900ec8693d
Andrew Smith <andrewchristophersmith@gmail.com>
2016-10-02 10:36:18 -0700

    Don't hardcode input and output sounds

--- examples/louder.rs
+++ examples/louder.rs
@@ -11,18 +11,22 @@ use std::cmp::Ordering;
 use soundsym::*;
 use getopts::Options;
 
+/// Arranges the phonemes in a sound file in order of increasing loudness.
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let program = args[0].clone();
     let mut opts = Options::new();
     opts.optopt("d", "depth", "depth of analysis trie", "DEPTH");
     opts.optopt("t", "threshold", "threshold for segmentation", "THRESHOLD");
-    let matches = match opts.parse(&args[1..]) {
-        Ok(m) => { m }
-        Err(f) => { panic!(f.to_string()) }
-    };
+    opts.reqopt("s", "sound", "path to input sound file", "SOUND");
+    opts.reqopt("o", "output", "path to output sound file", "OUTPUT");
 
-    let path = Path::new("data/we_remember_mono.wav");
+    let matches = opts.parse(&args[1..]).unwrap_or_else(|f| panic!(f.to_string()));
+
+    let input_file = matches.opt_str("s").unwrap();
+    let output_path = matches.opt_str("o").unwrap();
+
+    let path = Path::new(&input_file);
+
     let splits = Partitioner::from_path(&path).unwrap()
         .threshold(matches.opt_str("t")
                .and_then(|s| s.parse::<usize>().ok()).unwrap_or(3))
@@ -30,7 +34,7 @@ fn main() {
                .and_then(|s| s.parse::<usize>().ok()).unwrap_or(4))
         .partition().unwrap();
 
-    let mut input = hound::WavReader::open(&path).unwrap();
+    let mut input = hound::WavReader::open(&path).expect("Could not open input file");
     let spec = input.spec();
     let sample_rate = input.spec().sample_rate;
     let bits_down: u32 = 32 - spec.bits_per_sample as u32;
@@ -44,13 +48,14 @@ fn main() {
 
     sounds.sort_by(|a, b| a.max_power().partial_cmp(&b.max_power()).unwrap_or(Ordering::Equal));
 
-    let mut output = hound::WavWriter::create(&Path::new("output.wav"), spec).unwrap();
+    let mut output = hound::WavWriter::create(&Path::new(&output_path), spec).unwrap();
     for sound in sounds {
         println!("sound: {}", sound);
         for sample in sound.samples() {
-            output.write_sample((sample * i32::max_value().wrapping_shr(bits_down) as f64) as i32);
+            output.write_sample((sample * i32::max_value().wrapping_shr(bits_down) as f64) as i32)
+                .expect("Could not write sample");
         }
     }
-    output.finalize();
+    output.finalize().expect("Could not finalize file");
 }
 
